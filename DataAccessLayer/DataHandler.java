1eaafa85a3e5534027251f3b78509b4ab29d0dc6
Phumelele-Mthimunye <mrgeminiaura@gmail.com>
2021-02-28 17:09:29 +0200

    /\

---
+++ DataAccessLayer/DataHandler.java
@@ -0,0 +1,9 @@
+package DataAccessLayer;
+
+public class DataHandler {
+    public DataHandler(){}
+
+    String connect = "Data Source =.; Initial Catalog= DeliciousCateringDB; Integrated Security = SSPI";
+    
+    
+}
